86b02926b792b087d9059223bc2b9303311ff6ad
SmallMushroom-offical <jxk370@126.com>
2024-05-08 21:56:36 +0800

    feat:使用JavaScript提示框代替了之前的html提示;删除了一部分跳转网页的无用的前缀

---
+++ confirm.js
@@ -0,0 +1,12 @@
+function showConfirm(version) {
+    if (version == "legacy") {
+        var confirmmessage="确定要前往网盘下载远古版（不是指Minecraft远古版！）MineOptimiz吗？其中一些上传较晚的文件是最近才找回的";
+    } else {
+        var confirmmessage="警告！该版本已停止支持，将不会收到任何更新。你确定要下载吗？";
+    }
+    if (confirm(confirmmessage)) {
+        return true;
+    } else {
+        return false;
+    }
+}
