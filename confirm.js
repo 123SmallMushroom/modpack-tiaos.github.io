931f3b7a7af1bece25e2106a2dbcc6fc891ab306
SmallMushroom-offical <jxk370@126.com>
2024-05-09 07:00:12 +0800

    feat:可以在进入Github仓库时选择历史的仓库

--- confirm.js
+++ confirm.js
@@ -10,3 +10,15 @@ function showConfirm(version) {
         return false;
     }
 }
+function showPrompt() {
+    var githubrepo=prompt("请选择一个仓库：1.123SmallMushroom/MineOptimiz 2.SmallMushroom-offical/MineOptimiz-Next 3.MineOptimiz-Team/MineOptimiz-3rd");
+    if (githubrepo == "1") {
+        window.location.href = "https://github.com/123SmallMushroom/MineOptimiz";
+    } else if (githubrepo == "2") {
+        window.location.href = "https://github.com/SmallMushroom-offical/MineOptimiz-Next";
+    } else if (githubrepo == "3") {
+        window.location.href = "https://github.com/MineOptimiz-Team/MineOptimiz-3rd"
+    } else {
+        alert("请输入正确的数字！")
+    }
+}
