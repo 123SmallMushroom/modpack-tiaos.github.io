0c5159ea80c9a1d710e01665cab0384c4a37d71b
smallmushroomovo <jxk370@126.com>
2024-10-07 12:06:15 +0800

    update

--- mineoptimizindex.js
+++ mineoptimizindex.js
@@ -1,6 +1,6 @@
 function showConfirm(version) {
     if (version == "legacy") {
-        var confirmmessage="确定要前往网盘下载远古版（不是指Minecraft远古版！）MineOptimiz吗？其中一些上传较晚的文件是最近才找回的";
+        var confirmmessage="确定要前往网盘下载旧版MineOptimiz吗？";
     } else {
         var confirmmessage="警告！该版本已停止支持，将不会收到任何更新。你确定要下载吗？";
     }
